d2b4fd06011b0c750ad50187cdd4aa03e4ba3539
Aditya Naidu <adityanaidu16344@gmail.com>
2025-02-23 18:50:44 -0500

    deploy

--- altitude-landing/next.config.js
+++ altitude-landing/next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-    output: 'export',
-    basePath: '/altitude-landing',
-    images: {
-      unoptimized: true,
-    },
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/altitude-landing' : '',
-  }
-  
-  module.exports = nextConfig
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig
