7dabe4e6bf5c54a37ea24ebd027c97f76d85baaf
Aditya Naidu <adityanaidu16344@gmail.com>
2025-02-23 19:36:05 -0500

    working deploy?

--- altitude-landing/next.config.js
+++ altitude-landing/next.config.js
@@ -1,23 +1,10 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = ''
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-  assetPrefix = `/${repo}`
-  basePath = `/${repo}`
-}
-
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-}
-
-module.exports = nextConfig
+    output: 'export',
+    trailingSlash: true,
+    images: {
+      unoptimized: true,
+    },
+  }
+  
+  module.exports = nextConfig
