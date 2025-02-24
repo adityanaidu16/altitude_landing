a777dc25ea0948d82c3b339e704863b1966f5ea1
Aditya Naidu <adityanaidu16344@gmail.com>
2025-02-23 19:14:41 -0500

    deploy

--- altitude-landing/next.config.js
+++ altitude-landing/next.config.js
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    trailingSlash: true,
     images: {
       unoptimized: true,
     },
-    // Only add basePath and assetPrefix when deploying to GitHub Pages
-    ...(process.env.GITHUB_ACTIONS && {
-      basePath: '/altitude-landing',
-      assetPrefix: '/altitude-landing',
-    })
   }
   
   module.exports = nextConfig
